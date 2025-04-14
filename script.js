336758aa3df8e73a8eaa630bc2a81b07ba89e9f6
yeva2506 <yerudenko@46.ukr.education>
2025-04-14 12:46:12 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+let currentSlide = 0;
+const slides = document.querySelectorAll(".carousel-img");
+
+function showSlide(index) {
+  slides.forEach((slide, i) => {
+    slide.classList.toggle("active", i === index);
+  });
+}
+
+setInterval(() => {
+  currentSlide = (currentSlide + 1) % slides.length;
+  showSlide(currentSlide);
+}, 3000);
